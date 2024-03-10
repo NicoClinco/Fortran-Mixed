1e9560fd8df634f0834ff5ed581f49edcabfbece
nclinco <nicoclinco@gmail.com>
2024-03-10 23:10:00 +0100

    Added fourier derivative in 2d

--- SPECTRAL_METHODS/Interpolant/src/apps/testWrapper.f90
+++ SPECTRAL_METHODS/Interpolant/src/apps/testWrapper.f90
@@ -16,7 +16,13 @@ program main
   !real,parameter :: pi = 3.1416
 
   type (FourierInterp2d) :: Interpolant2d;
-  real(kind=8) :: y_int;
+  type (FourierDerivative2d) :: Derivative2d;
+  real(kind=8) :: y_int,grady_x;
+
+  !-----------TESTING-----------------------
+  real(kind=8):: intpoints(10,2)
+  real(kind=8):: physSol(10) 
+  
   character(20) :: separator_line;
   separator_line(:) = '********************';
 
@@ -37,20 +43,20 @@ program main
 
   input_array = sin(x)*sin(y); !Create the input array
   org_array = input_array;
-  num(:) = n;
-  call fft(output_array,input_array,num)
-  input_array =output_array; !copy
-  call ifft(output_array,input_array,num)
-  error_array = output_array - org_array
+  !num(:) = n;
+  !call fft(output_array,input_array,num)
+  !input_array =output_array; !copy
+  !call ifft(output_array,input_array,num)
+  !error_array = output_array - org_array
 
   !do i = 1,n
   !   print '(10(f6.3,2x))',output_array(i,:)
   !enddo
-  print '(2A)',separator_line,separator_line
+  !print '(2A)',separator_line,separator_line
   !do i = 1,n
   !   print '(10(e12.6,2x))',error_array(i,:)
   !enddo
-  print '(A,1X,e12.6)','Absolute-error-interpolation:',sum(real(output_array)-(org_array));
+  !print '(A,1X,e12.6)','Absolute-error-interpolation:',sum(real(output_array)-(org_array));
 
 
   print '(2A)',separator_line,separator_line
@@ -84,13 +90,65 @@ program main
 
   print '(A)','...Creating the interpolant...'
   
-  Interpolant2d = FourierInterp2d_(n,n,real(org_array,8))
+  Interpolant2d = FourierInterp2d_(n,n,real(org_array,8));
 
+  Derivative2d = DerFourierInterp2d_(n,n,real(org_array,8));
+
+  call FwdTransform2d(Derivative2d)
+  
   call FwdTransform2d(Interpolant2d)
 
-  call FourierInterpolate2D_scalar(Interpolant2d,[0.2_rk,0.2_rk],y_int)
-
+  !call FourierInterpolate2D_scalar(Interpolant2d,[0.2_rk,0.2_rk],y_int)
+  call Derivative2d%interpolate2ds([0.2_rk,0.2_rk],y_int)
   
   print '(A,1x,e12.6)','Interpolated value at x,y = (0.2,0.2):',y_int
+
+  !Initialize the discrete coefficients
+  call derFwdTransform2d_x(Derivative2d)
+
+  !Compute the derivative:
+  call FourierGradX_scalar(Derivative2d,[0.2_rk,0.2_rk],grady_x)
+
+  write(*,'(A,1X,e10.5)')'Derivative in (x,y) = (0.2,0.2):',grady_x
+  write(*,'(A,1X,e10.5)')'True-Derivative in (x,y) = (0.2,0.2):',sin(0.2_rk)*cos(0.2_rk)
+
+  if(abs(grady_x-sin(0.2_rk)*cos(0.2_rk)) .le. 1.0E-7) then
+     write(*,'(3A)') separator_line,'Test on the derivative pointwise passed',separator_line
+  endif
   
+  do i=1,10
+     intpoints(i,1) = real(i-1,8)/10.0_rk
+     intpoints(i,2) = 10.0_rk-real(i-1,8)/10_rk
+     physSol(i) = sin(intpoints(i,1))*sin(intpoints(i,2))
+  enddo
+
+  !call testInterpolation2Dv(Derivative2d,intpoints,physSol)
+contains
+
+  !>@brief Test the interpolation properties
+  !        by interpolating to a set
+  !                           of points 
+  !>@param [in] Iobj:         The interpolation object
+  !>@param [in] physPoints:   The physical points obtained
+  !>@param [in] physSolution: 
+  subroutine testInterpolation2Dv(Iobj,physPoints,physSolution)
+    implicit none
+    class(FourierInterp2d),intent(in) :: Iobj;
+    real(rk),intent(in) ::physPoints(:,:); !The row-vector points
+    real(rk),intent(in) :: physSolution(:) !The row-vector solution
+    integer :: nps;
+    real(rk) :: intrpSolution(size(physSolution))
+
+    call Iobj%Interpolate2dv(physPoints,intrpSolution);
+
+    
+    
+    write(*,'(A)')'...Test the interpolation properties of the Fourier2d...'
+    do nps = 1,size(physSolution)
+       print *,nps
+       write(*,'(2(e12.5,1x)," Error:",1x,e12.5)') physPoints(nps,:), &
+            abs(intrpSolution(nps) - physSolution(nps))
+    enddo
+    
+  end subroutine testInterpolation2Dv
 end program main
--- SPECTRAL_METHODS/Interpolant/src/modules/Interp2DClass.f90
+++ SPECTRAL_METHODS/Interpolant/src/modules/Interp2DClass.f90
@@ -3,6 +3,10 @@ module FourierInterp2dClass
   use fftpack_kind, only: rk
   implicit none
 
+  
+  interface Interpolate2d
+     module procedure Fourierinterpolate2D_scalar,Fourierinterpolate2D_vector
+  end interface Interpolate2d
   !Basic type for interpolate a 2d function.
   type FourierInterp2d
      !private
@@ -12,16 +16,49 @@ module FourierInterp2dClass
      logical :: m_transformed = .false.;
    contains
      procedure FwdTransform2d
-     procedure FourierInterpolate2D_scalar
-     
+     procedure :: interpolate2ds => FourierInterpolate2D_scalar
+     procedure :: interpolate2dv => FourierInterpolate2D_vector
   end type FourierInterp2d
+
+
+  type, extends(FourierInterp2d) :: FourierDerivative2d
+     !private
+     complex(rk),allocatable :: dydxHat(:,:);
+     complex(rk),allocatable :: dydyHat(:,:);
+     logical :: m_derivtransformed;
+   contains
+     !Note that the procedure are inherited from the base class
+  end type FourierDerivative2d
 
   real(rk),parameter :: pi =  3.14159265358979323846;!pi
   complex,parameter :: ju = (0.0,1.0);   !img unity
 
 contains
 
-  !>@brief Basic constructor
+  !>@brief Basic constructor for the derivative object
+  !@param Nx: The number of points along x
+  !@param Ny: The number of points along y
+  !@param xy: The matrix which contains the fitted points for
+  !           the function.
+  function DerFourierInterp2d_(Nx,Ny,fxy) result (Dobj)
+    implicit none
+    integer,intent(in) :: Nx,Ny; !Number of discrete values.
+    real(rk),intent(in) :: fxy(Nx,Ny); !Vector of discrete values.
+    type (FourierDerivative2d) :: Dobj; !Interpolation object
+
+    !Construct the object by assuming
+    !that the fourier interpolant is y.
+
+    allocate(Dobj%y(Nx,Ny),Dobj%yHat(Nx,Ny) &
+         ,Dobj%dydxHat(Nx,Ny),Dobj%dydyHat(Nx,Ny));
+
+    Dobj%y = fxy
+    Dobj%m_transformed = .false.
+    Dobj%m_derivtransformed = .false.
+  end function DerFourierInterp2d_
+  !---------------------------------------------------------
+
+  !>@brief Basic constructor for the interpolation object
   !@param Nx: The number of points along x
   !@param Ny: The number of points along y
   !@param xy: The matrix which contains the fitted points for
@@ -40,19 +77,89 @@ contains
     Iobj%y = fxy
     Iobj%m_transformed = .false.
   end function FourierInterp2d_
-!----------------------------------------------------
+!-----------------------------------------------------------
+
   !>@brief Performs the 2d transform for the object
   !and store it in yHat
   subroutine FwdTransform2d(Iobj)
     implicit none
     class (FourierInterp2d) :: Iobj
     call fft(Iobj%yHat,Iobj%y,shape(Iobj%y));
+    Iobj%m_transformed = .true.
   end subroutine FwdTransform2d
+!------------------------------------------------------------
 
+  !>@brief Initialize the coefficients for computing
+  !the derivative along the x direction:
+  subroutine derFwdTransform2d_x(Dobj)
+    use fftpack, only : fftfreq !Use the frequencies
+    implicit none
+    integer :: i,nx
+    class (FourierDerivative2d),intent(inout) :: Dobj
+    if (Dobj%m_transformed .eqv. .false.) then
+       call FwdTransform2d(Dobj)
+    endif
+    
+    nx = size(Dobj%y,1)
+    do i=1,size(Dobj%y,2)
+       Dobj%dydxHat(:,i) = ju*fftfreq(nx)*Dobj%yHat(:,i)
+    enddo
+      
+  end subroutine derFwdTransform2d_x
+  !----------------------------------------------------
+
+  subroutine derFwdTransform2d_y(Dobj)
+    use fftpack, only : fftfreq !Use the frequencies
+    implicit none
+    integer :: i,ny
+    class (FourierDerivative2d),intent(inout) :: Dobj
+    if (Dobj%m_transformed .eqv. .false.) then
+       call FwdTransform2d(Dobj)
+    endif
+    
+    ny = size(Dobj%y,2)
+    do i=1,size(Dobj%y,1)
+       Dobj%dydyHat(i,:) = ju*fftfreq(ny)*Dobj%yHat(i,:)
+    enddo
+      
+  end subroutine derFwdTransform2d_y
+  
+  !--------------------------------------------------------
+  !>@brief Get the derivative in a specified 2d point
+  !@param[in] Dobj The derivative object
+  !@param[in] xy Specific point where we want the gradient
+  !@param[]
+  subroutine FourierGradX_scalar(Dobj,xy,dyInt)
+    use fftpack, only : fftfreq !Use the frequencies
+    implicit none
+    class (FourierDerivative2d),intent(inout) :: Dobj
+    real(rk),intent(in) :: xy(:)
+    real(rk),intent(out) :: dyInt
+
+    integer :: i,n(2)
+    complex(rk),allocatable :: expK(:,:),expL(:,:);
+
+    n = shape(Dobj%y)
+     
+    allocate(expK(n(1),n(2)),expL(n(1),n(2))); !allocate
+    
+    !Get the discrete exponential in the specific points:
+    do i =1,n(2)
+       expK(:,i) = exp(ju*xy(1)*fftfreq(n(1)))
+
+    enddo
+    do i=1,n(1)
+       expL(i,:) = exp(ju*xy(2)*fftfreq(n(2)))
+    enddo
+    dyInt = real(sum(Dobj%dydxHat*expK*expL))/(n(1)*n(2));
+  end subroutine FourierGradX_scalar
+!------------------------------------------------------------
+  
+  
   !>@brief Interpolate in a specified point
-  !>@param [in] xy: An array which contains
+  !@param [in] xy: An array which contains
   ! the coordinates where we want to interpolate
-  !>@param [in] 
+  !@param [in] 
   subroutine FourierInterpolate2D_scalar(Iobj,xy,yInt)
     use fftpack, only : fftfreq !Use the frequencies
     implicit none
@@ -76,10 +183,11 @@ contains
     enddo
     yInt = real(sum(Iobj%yHat*expK*expL))/(n(1)*n(2));
   end subroutine FourierInterpolate2D_scalar
-
+  !----------------------------------------------------------------
+  
   !>@brief Interpolate to a set of interpolation points
-  !>@param[in] Iobj
-  !>@param[in] xy(nPoints,2) a row vector which contains
+  !@param[in] Iobj
+  !@param[in] xy(nPoints,2) a row vector which contains
   !the coordinates
   !@out param yInt [out] The interpolated values.
   subroutine FourierInterpolate2D_vector(Iobj,xy,yInt)
