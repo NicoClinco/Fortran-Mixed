ab6df3c183fdb74f33cb9de56d7bbfb1ed123b82
nclinco <nicoclinco@gmail.com>
2024-03-24 23:44:11 +0100

    Added first explicit solver [to test]

--- SPECTRAL_METHODS/FourierSpectralMethods/src/apps/testSpectralSolver.f90
+++ SPECTRAL_METHODS/FourierSpectralMethods/src/apps/testSpectralSolver.f90
@@ -9,13 +9,38 @@ program test1dFourier
   implicit none
 
   integer :: numModes
-  real(kind=8) :: a,c
+  real(kind=8) :: h
+  real(kind=8),allocatable :: x(:)
+  real(kind=8) :: alpha,epsilon !The convection coefficients.
   complex(kind=8),allocatable :: convMatrix(:,:)
   complex(kind=8),allocatable :: diffMatrix(:,:)
   real(8),parameter :: pi = 3.14159265358979323846
+  complex(kind=8),allocatable :: uHatStages(:,:) !Solution at every stage [explicit stage]
+  complex(kind=8),allocatable :: uHatTimes(:,:) !Solution at every time instance
+  complex(kind=8),allocatable :: uPhysTimes(:,:) !Solution [physical] at every time instance
+  
+  !Time-stepping scheme tableu:
+  integer :: numStages = 4
+  real(kind=8) :: A(4,4)
+  real(kind=8) :: b(4)
+  real(kind=8) :: c(4)
+  integer :: numSteps,step,stage,i,j
+  real(kind=8) :: tstart,tend,dt,curr_time
+  !Time advancing stuffs:
+  tstart = 0.0
+  tend   = 2.0
+  numSteps= 50
+  dt = (tend-tstart)/real(numSteps)
 
-  convMatrix = FourierAdvectionMatrix(6);
-  diffMatrix = FourierDiffusionMatrix(6);
+  numModes = 32
+  allocate( uHatTimes(numSteps,numModes), uPhysTimes(numSteps,numModes), &
+       uHatStages(numStages,numModes))
+
+  alpha = 0.5
+  epsilon = 0.0
+  
+  convMatrix = FourierAdvectionMatrix(numModes,alpha);
+  diffMatrix = FourierDiffusionMatrix(numModes,epsilon);
 
   write(*,'(A)')'-------------------------------------'
   call print_matrix(convMatrix)
@@ -24,51 +49,119 @@ program test1dFourier
 
   write(*,'(A)')'=========> STARTING THE TIME-STEPPING SCHEME========>'
 
+  !Construct the butcher tableu:
+  call ConstructRK4(A,b,c,4)
+
+
+
+  !Mesh:
+  allocate(x(numModes))
+  h = 2.0*pi/real(numModes,8)
+  do i=1,numModes
+     x(i) = (i-1)*h
+  enddo
+  curr_time = tstart
+  step=1
+  uPhysTimes(step,:) = u0(x)
+  
+  !Initial condition   
+  call fft(uHatTimes(step,:),uPhysTimes(step,:))
+  
+  do while(curr_time<tend)
+
+     uHatStages(1,:) = uHatTimes(step,:)
+     do stage = 2,numStages
+        uHatStages(stage,:) = uHatTimes(step,:)
+        do j = 1,stage-1
+           uHatStages(stage,:) = uHatStages(stage,:)+dt*A(stage,j)*matmul(convMatrix,uHatStages(j,:))
+        enddo
+     enddo !end stages
+     uHatTimes(step+1,:) = uHatTimes(step,:)
+     do stage = 1,numStages
+        uHatTimes(step+1,:) = uHatTimes(step+1,:)+dt*b(stage)*matmul(convMatrix,uHatStages(stage,:))
+     enddo
+     call ifft(uPhysTimes(step+1,:),uHatTimes(step+1,:))
+     step=step+1
+     curr_time = curr_time + dt 
+  enddo
   
 
   write(*,'(A)')'=========> ENDING THE TIME-STEPPING SCHEME==========>'
   
 contains
-  !> Assemble the advection matix given
+
+  !> The initial solution here:
+  !!
+  !!
+  !!
+  function u0(x)
+    implicit none
+    real,allocatable :: u0(:);
+    real(8) :: x(:)
+    allocate(u0(size(x)))
+    u0 = exp(-1.0*(x-pi)**2)
+  end function u0
+  !> Assemble the advection matrix given
   !! the number of fourier modes
   !! 
+  !! 
   !!
-  !!
-  function FourierAdvectionMatrix(modes) result(F)
+  function FourierAdvectionMatrix(modes,alpha) result(F)
     use SCIFOR, only : diag
     use fftpack, only : fftfreq
     implicit none
+    real(kind=8),intent(in) :: alpha;
     integer,intent(in) :: modes;
     complex(kind=8) :: ks(modes) !The frequencies
     complex(kind=8),allocatable :: F(:,:)
     complex(kind=8) :: ju = (0.0,1.0) !Unitary vector
     ks = cmplx(fftfreq(modes))!Get the frequencies
-    !allocate(F(modes,modes))
-    F = diag(ks);
+    F = diag(ju*alpha*ks);
 
   end function FourierAdvectionMatrix
 
   !> Evaluate the diffusion matrix
   !!
   !!
-  function FourierDiffusionMatrix(modes) result(Fsquare)
+  function FourierDiffusionMatrix(modes,epsilon) result(Fsquare)
     use SCIFOR, only : diag
     use fftpack, only : fftfreq
     implicit none
+    real(kind=8),intent(in) :: epsilon
     integer,intent(in) :: modes;
     complex(kind=8) :: ks(modes) !The frequencies
     complex(kind=8),allocatable :: Fsquare(:,:)
 
     !allocate(Fsquare(modes,modes))
     ks = cmplx(fftfreq(modes))!Get the frequencies
-    Fsquare = diag(-ks*ks);
+    Fsquare = diag(-epsilon*ks*ks);
   end function FourierDiffusionMatrix
 
   !> Timestepping scheme
   !!
   !!
   !!
-  subroutine timeSteppingScheme()
+  subroutine ConstructRK4(A,b,c,numStages)
+    integer,intent(in) :: numStages
+    real(kind=8),intent(inout) :: A(numStages,numStages)
+    real(kind=8),intent(inout):: b(numStages)
+    real(kind=8),intent(inout):: c(numStages)
+
+    A(:,:) = 0.0
+    A(2,1) = 0.5
+    A(3,2) = 0.5
+    A(4,3) = 1.0
+
+    b(1) = 1.0/6.0
+    b(2) = 1.0/3.0
+    b(3) = 1.0/3.0
+    b(4) = 1.0/6.0
+
+    c(1) = 0.0
+    c(2) = 0.5
+    c(3) = 0.5 
+    c(4) = 1.0
     
-  end subroutine timeSteppingScheme
+
+  end subroutine ConstructRK4
 end program test1dFourier
