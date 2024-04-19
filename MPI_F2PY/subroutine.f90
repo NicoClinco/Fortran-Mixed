8f51566f446db21606b56d917b36974b3759bd71
nclinco <nicoclinco@gmail.com>
2024-04-19 08:34:32 +0200

    Inserted the example on bindings from fortran to python with MPI

---
+++ MPI_F2PY/subroutine.f90
@@ -0,0 +1,18 @@
+subroutine simple
+include 'mpif.h'
+
+integer numtasks, rank, ierr, rc
+
+call MPI_INIT(ierr)
+if (ierr .ne. MPI_SUCCESS) then
+print *,'Error starting MPI program. Terminating.'
+call MPI_ABORT(MPI_COMM_WORLD, rc, ierr)
+end if
+
+call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+call MPI_COMM_SIZE(MPI_COMM_WORLD, numtasks, ierr)
+print *, 'Number of tasks=',numtasks,' My rank=',rank
+
+call MPI_FINALIZE(ierr)
+
+end subroutine
