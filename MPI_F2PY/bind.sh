4f3217529a32d0cfbae7e32cebe1f5f10a4cc751
nclinco <nicoclinco@gmail.com>
2024-04-23 22:42:41 +0200

    Added definitive binding

--- MPI_F2PY/bind.sh
+++ MPI_F2PY/bind.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-f2py --f90exec=mpifort --f77exec=mpifort -c subroutine.f90 -m simpleMPI
-
+f2py --f90exec=mpif90 --f77exec=mpif90 -c subroutine.f90 -m simpleMPI \
+     -L/usr/lib/x86_64-linux-gnu/ -lmpi
+#f2py --f90exec=mpif90 --f77exec=mpif90 -I
+#mpiexec -n 2 python3 mpi4py_example.py
 
